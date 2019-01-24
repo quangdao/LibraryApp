754522fe41b302ed9f88d178695230b318eef3d5
Penh Lenh <penhlenh@gmail.com>
2019-01-24 11:16:22 -0600

    Add sample data according to requirements

--- src/controller/PrintCheckoutRecordController.java
+++ src/controller/PrintCheckoutRecordController.java
@@ -3,14 +3,8 @@ package controller;
 import business.Checkout;
 import business.CheckoutEntry;
 import dataaccess.ObjectReader;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import viewModel.CheckoutRecordEntryViewModel;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class PrintCheckoutRecordController {
--- src/dataaccess/SampleData.java
+++ src/dataaccess/SampleData.java
@@ -17,9 +17,15 @@ public class SampleData {
         Member member = new Member("1", "Quang", "Dao", "22456", add1);
         Member member2 = new Member("2", "Penh", "Lim", "34556346", add2);
         Member member3 = new Member("3", "Jack", "Ma", "565869", add3);
+        Member member4 = new Member("4", "Paul", "Chris", "323323", add1);
 
-        ObjectWriter.Output("members", new ArrayList<Member>(){
-            { add(member); add(member2); add(member3); }
+        ObjectWriter.Output("members", new ArrayList<Member>() {
+            {
+                add(member);
+                add(member2);
+                add(member3);
+                add(member4);
+            }
         });
 
         List<User> users = Arrays.asList(
@@ -27,7 +33,7 @@ public class SampleData {
                 new User("super", "super", Role.SUPERADMIN),
                 new User("librarian", "librarian", Role.LIBRARIAN)
         );
-       
+
         ObjectWriter.Output("users", users);
 //
 //        Address add1 = new Address("9387 NeverEnd 4th Street", "Fairfield", "IA", "52557");
@@ -49,30 +55,59 @@ public class SampleData {
 //               
 //        );
 //        ObjectWriter.Output("books", books);
-        
+
         // Sample books record
         Address address = new Address("1000 N4", "FairField", "Iowa", "52557");
         List<Author> authors = Arrays.asList(new Author("1", "Joe", "Brendy", "641-651-24XX", address, "granted", "Volleyball lover"));
         Book book1 = new Book("book1", "Core Java For Impatient", "isbn1", DEFAULT_CHECKOUT_LENGTH, authors);
-//        Book book2 = new Book("book2", "Fundamental Java Language", "isbn2", authors);
-//        Book book3 = new Book("book3", "Advanced Java Programming", "isbn3", authors);
-//        Book book4 = new Book("book4", "Spring Essential", "isbn4", authors);
+        Book book2 = new Book("book2", "Fundamental Java Language", "isbn2", DEFAULT_CHECKOUT_LENGTH, authors);
+        Book book3 = new Book("book3", "Advanced Java Programming", "isbn3", DEFAULT_CHECKOUT_LENGTH, authors);
+        Book book4 = new Book("book4", "Spring Essential", "isbn4", DEFAULT_CHECKOUT_LENGTH, authors);
+
+        book1.addBookCopy(new BookCopy(book1));
+        book1.addBookCopy(new BookCopy(book1));
+
+        book2.addBookCopy(new BookCopy(book2));
+        book2.addBookCopy(new BookCopy(book2));
+
         book1.getBookCopy().get(0).setAvailable(false);
-        List<Book> books = new ArrayList<>(Arrays.asList(book1));
+        book1.getBookCopy().get(1).setAvailable(false);
+        book1.getBookCopy().get(2).setAvailable(false);
+
+        book2.getBookCopy().get(0).setAvailable(false);
+        book2.getBookCopy().get(1).setAvailable(false);
+        book2.getBookCopy().get(2).setAvailable(false);
+
+        List<Book> books = new ArrayList<>(Arrays.asList(book1, book2, book3, book4));
         ObjectWriter.Output("books", books);
 
         // Sample checkout record
         LocalDate dueDate = LocalDate.now().plusDays(DEFAULT_CHECKOUT_LENGTH);
 
-        List<CheckoutEntry> checkoutEntries = Arrays.asList(new CheckoutEntry(book1.getBookCopy().get(0), LocalDate.now(), dueDate));
+        List<CheckoutEntry> checkoutEntries = Arrays.asList(new CheckoutEntry(book1.getBookCopy().get(0), LocalDate.now().minusDays(6), LocalDate.now().minusDays(1)));
+        List<CheckoutEntry> checkoutEntries1 = Arrays.asList(new CheckoutEntry(book1.getBookCopy().get(1), LocalDate.now(), dueDate));
+        List<CheckoutEntry> checkoutEntries2 = Arrays.asList(new CheckoutEntry(book2.getBookCopy().get(0), LocalDate.now().minusDays(7), LocalDate.now().minusDays(2)));
+        List<CheckoutEntry> checkoutEntries3 = Arrays.asList(new CheckoutEntry(book2.getBookCopy().get(1), LocalDate.now(), dueDate));
+
+        List<CheckoutEntry> checkoutEntries4 = Arrays.asList(new CheckoutEntry(book1.getBookCopy().get(2), LocalDate.now(), dueDate));
+        List<CheckoutEntry> checkoutEntries5 = Arrays.asList(new CheckoutEntry(book2.getBookCopy().get(2), LocalDate.now(), dueDate));
+
         Checkout checkout = new Checkout(member, checkoutEntries);
-        List<Checkout> checkouts = new ArrayList<Checkout>(){{add(checkout);}};
+        Checkout checkout1 = new Checkout(member, checkoutEntries1);
+        Checkout checkout2 = new Checkout(member2, checkoutEntries2);
+        Checkout checkout3 = new Checkout(member2, checkoutEntries3);
+        Checkout checkout4 = new Checkout(member3, checkoutEntries4);
+        Checkout checkout5 = new Checkout(member4, checkoutEntries5);
+
+        List<Checkout> checkouts = new ArrayList<Checkout>() {{
+            add(checkout);
+            add(checkout1);
+            add(checkout2);
+            add(checkout3);
+            add(checkout4);
+            add(checkout5);
+        }};
 
         ObjectWriter.Output("checkouts", checkouts);
-
-        List<User> user4 = (List<User>) ObjectReader.getObjectByFilename("users");
-        for (User userobj : user4) {
-            System.out.println(userobj);
-        }
     }
 }
